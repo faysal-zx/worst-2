9aa733c8d6740989699c896497d35277fe830c43
faysal-zx <zxfaysalgrp@gmail.com>
2020-09-06 23:29:41 +0800

    Updates

---
+++ precache-manifest.751b61ba46f67fef940f3977c5354e62.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03745bf0fb09be7be13e493455824db3",
+    "url": "/New_Folder(3)/index.html"
+  },
+  {
+    "revision": "8725b91a6c1d860365e7",
+    "url": "/New_Folder(3)/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "307867521441997aee9d",
+    "url": "/New_Folder(3)/static/js/2.174b0096.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/New_Folder(3)/static/js/2.174b0096.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8725b91a6c1d860365e7",
+    "url": "/New_Folder(3)/static/js/main.12d5aceb.chunk.js"
+  },
+  {
+    "revision": "2f34b00aec32b9f314bb",
+    "url": "/New_Folder(3)/static/js/runtime-main.e5e0e4d0.js"
+  }
+]);
